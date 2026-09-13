8723ff0ba162e050e69a7ed1466cae9676e1a6b8
vivizac <ipro721@gmail.com>
2026-09-13 12:01:46 +0900

    fix: use stable time-row markup anchors

--- tmp_source_own_student_time_rows.py
+++ tmp_source_own_student_time_rows.py
@@ -5,27 +5,27 @@ text = ui.read_text()
 
 replacements = [
     (
-        "        + '<div id=\"elementaryStudentLessonDayField\" class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryStudentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'\n        \n        + '<div id=\"elementaryStudentGroupField\"",
-        "        + '<div id=\"elementaryStudentLessonDayField\" class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryStudentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'\n        + '<div id=\"elementaryStudentLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"elementaryStudentLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"elementaryStudentLessonTimeToggleRowInput\"></div>'\n        + '<div id=\"elementaryStudentGroupField\""
+        "        + '<div id=\"elementaryStudentLessonDayField\" class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryStudentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'",
+        "        + '<div id=\"elementaryStudentLessonDayField\" class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryStudentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'\n        + '<div id=\"elementaryStudentLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"elementaryStudentLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"elementaryStudentLessonTimeToggleRowInput\"></div>'"
     ),
     (
-        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"studentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"studentLessonDayInput\" type=\"hidden\"></div>'\n        \n        + '</div>'",
-        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"studentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"studentLessonDayInput\" type=\"hidden\"></div>'\n        + '<div id=\"studentLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"studentLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"studentLessonTimeToggleRowInput\"></div>'\n        + '</div>'"
+        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"studentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"studentLessonDayInput\" type=\"hidden\"></div>'",
+        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"studentLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"studentLessonDayInput\" type=\"hidden\"></div>'\n        + '<div id=\"studentLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"studentLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"studentLessonTimeToggleRowInput\"></div>'"
     ),
     (
-        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'\n        \n        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">그룹</div>'",
-        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'\n        + '<div id=\"elementaryLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"elementaryLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"elementaryLessonTimeToggleRowInput\"></div>'\n        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">그룹</div>'"
+        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'",
+        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"elementaryLessonDayToggleRow\" class=\"infoDayToggleRow\"></div></div>'\n        + '<div id=\"elementaryLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"elementaryLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"elementaryLessonTimeToggleRowInput\"></div>'"
     ),
     (
-        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"kinderLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"kinderLessonDayInput\" type=\"hidden\"></div>'\n        \n        + ''",
-        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"kinderLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"kinderLessonDayInput\" type=\"hidden\"></div>'\n        + '<div id=\"kinderLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"kinderLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"kinderLessonTimeToggleRowInput\"></div>'\n        + ''"
+        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"kinderLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"kinderLessonDayInput\" type=\"hidden\"></div>'",
+        "        + '<div class=\"kinderInfoModalField\"><div class=\"modalLabel\">요일</div><div id=\"kinderLessonDayToggleRow\" class=\"infoDayToggleRow\"></div><input id=\"kinderLessonDayInput\" type=\"hidden\"></div>'\n        + '<div id=\"kinderLessonTimeField\" class=\"kinderInfoModalField studentScheduleTimeField\"><div class=\"modalLabel\">시간</div><div id=\"kinderLessonTimeToggleRow\" class=\"infoTimeToggleRow\"></div><input type=\"hidden\" id=\"kinderLessonTimeToggleRowInput\"></div>'"
     ),
 ]
 
 for old, new in replacements:
     count = text.count(old)
     if count != 1:
-        raise SystemExit(f'markup anchor mismatch: {count} for {old[:90]!r}')
+        raise SystemExit(f'markup anchor mismatch: {count} for {old[:110]!r}')
     text = text.replace(old, new, 1)
 
 for required_id in [
